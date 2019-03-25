0dd2f7a09ff52f2e19d8f33216b52f44c6282cc0
njdevengine <newjerseydevelopment@gmail.com>
2019-03-25 10:10:33 -0400

    Update soup_to_text.py

--- soup_to_text.py
+++ soup_to_text.py
@@ -12,10 +12,12 @@ for i in range(0,length):
         print(webpage)
         my_soup.append(webpage.decode("utf-8"))
     except Exception as e: print(e)
-    
+        
+######CREATE SOME DIRECTORIES######
 import os
 path = os.getcwd()
 os.mkdir(path+"/my_files")
+os.mkdir(path+"/analysis")
     
 ######OUTPUT my_soup to text######
 import codecs
@@ -35,3 +37,61 @@ with open("combined.txt", 'wb') as outfile:
             continue
         with open(filename, 'rb') as readfile:
             shutil.copyfileobj(readfile, outfile)
+
+######HTML CLEANUP######
+
+import pandas as pd
+import matplotlib.pyplot as pt
+import numpy as np
+import re
+
+with open('combined.txt', 'r', encoding="utf-8") as myfile:
+    string = myfile.read().replace('\n', ' ')
+    string = string.lower()
+    
+regex = re.compile('[^a-zA-Z]')
+string = regex.sub(' ', string)
+
+words = string.split(" ")
+
+for word in words:
+    word.lower()
+
+words = list(filter(None, words))
+dfphrase1 = pd.DataFrame({"words" : words})
+grouped1 = dfphrase1["words"].value_counts()
+grouped1 = grouped1.to_frame()
+grouped1.rename(columns={"words":"count"})
+
+phrase = []
+num = len(words)-1
+for n in range(0,num):
+        phrase.append(words[n]+" "+words[n+1])
+dfphrase2 = pd.DataFrame({"phrase" : phrase})
+grouped2 = dfphrase2["phrase"].value_counts()
+grouped2 = grouped2.to_frame()
+grouped2.rename(columns={"phrase":"count"})
+
+phrase3 = []
+num = len(words)-2
+for n in range(0,num):
+        phrase3.append(words[n]+" "+words[n+1]+" "+words[n+2])
+        
+dfphrase3 = pd.DataFrame({"phrase3" : phrase3})
+grouped3 = dfphrase3["phrase3"].value_counts()
+grouped3 = grouped3.to_frame()
+grouped3.rename(columns={"phrase3":"count"})
+
+#dfphrase1.head()
+#dfphrase2.head()
+#dfphrase3.head()
+
+#grouped1.head()
+#grouped2.head()
+#grouped3.head()
+
+writer = pd.ExcelWriter('analysis/analysis_output.xlsx', engine = 'xlsxwriter')
+grouped1.to_excel(writer, sheet_name='grouped1')
+grouped2.to_excel(writer, sheet_name='grouped2')
+grouped3.to_excel(writer, sheet_name='grouped3')
+writer.save()
